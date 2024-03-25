dfb325916f326e91e4eb9bcb9db833790cd910bb
ksj0813 <tow369@naver.com>
2024-03-25 20:07:43 +0900

    Initial commit

---
+++ src/main/java/org/example/baekjoon1001/Main.java
@@ -0,0 +1,15 @@
+package org.example.baekjoon1001;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a,b;
+        a = s.nextInt();
+        b = s.nextInt();
+        System.out.println(a-b);
+
+    }
+}
