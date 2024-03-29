7fbdf78772ea066c8f1d62450aff3da1ff445e65
ksj0813 <tow369@naver.com>
2024-03-29 17:37:28 +0900

    baekjoon9498

---
+++ src/main/java/org/example/baekjoon9498/Main.java
@@ -0,0 +1,32 @@
+package org.example.baekjoon9498;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int num;
+
+        num = s.nextInt();
+
+        if(90 <= num)
+            System.out.println("A");
+        else
+            if(num>=80)
+                System.out.println("B");
+        else
+            if(70 <= num)
+                System.out.println("C");
+        else
+            if(60 <= num)
+                System.out.println("D");
+        else
+            if(num < 70)
+                System.out.println("F");
+
+        }
+
+    }
+
