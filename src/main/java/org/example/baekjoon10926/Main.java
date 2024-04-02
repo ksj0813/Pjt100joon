a3b8ac891e09eae34f03900b06c4fb341cb6a6cb
ksj0813 <tow369@naver.com>
2024-04-02 09:02:18 +0900

    baekjoon10926

---
+++ src/main/java/org/example/baekjoon10926/Main.java
@@ -0,0 +1,15 @@
+package org.example.baekjoon10926;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str ;
+        str = s.nextLine();
+         System.out.println( str + "??!" );
+
+
+
+    }
+}
