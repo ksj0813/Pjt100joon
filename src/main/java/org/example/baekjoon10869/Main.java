f9a2c7d83af5e8675aa1a110d1f0e847844e8942
ksj0813 <tow369@naver.com>
2024-04-02 09:03:11 +0900

    baekjoon10869

---
+++ src/main/java/org/example/baekjoon10869/Main.java
@@ -0,0 +1,21 @@
+package org.example.baekjoon10869;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a,b;
+
+        a = s.nextInt();
+        b = s.nextInt();
+
+        System.out.println( a+b);
+        System.out.println( a-b);
+        System.out.println( a*b);
+        System.out.println( a/b);
+        System.out.println( a%b);
+
+
+    }
+}
