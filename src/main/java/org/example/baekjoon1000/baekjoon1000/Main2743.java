6669426ddef9e99fd1eaddeb8a0c76808a10ddfc
ksj0813 <tow369@naver.com>
2024-04-05 16:55:11 +0900

    baekjoon.length

---
+++ src/main/java/org/example/baekjoon1000/baekjoon1000/Main2743.java
@@ -0,0 +1,14 @@
+package org.example.baekjoon1000.baekjoon1000;
+
+import java.util.Scanner;
+
+public class Main2743 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+        int len;
+        len = a.length();
+
+        System.out.println(len);
+    }
+}
