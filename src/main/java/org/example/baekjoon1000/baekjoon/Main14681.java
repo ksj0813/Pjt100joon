adfd8e129df7409ff57993d0b30d38ed42c43946
ksj0813 <tow369@naver.com>
2024-04-11 11:22:44 +0900

    baekjoon.14681

---
+++ src/main/java/org/example/baekjoon1000/baekjoon/Main14681.java
@@ -0,0 +1,25 @@
+package org.example.baekjoon1000.baekjoon;
+
+import java.util.Scanner;
+
+public class Main14681 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        if(a>0 && b>0){
+            System.out.print(1);
+        }
+        else if(a<0 && b>0){
+            System.out.print(2);
+        }
+        else if(a<0 && b<0){
+            System.out.print(3);
+        }
+        else if(a>0 && b<0){
+            System.out.print(4);
+        }
+    }
+}
