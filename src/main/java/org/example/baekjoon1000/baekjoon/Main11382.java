a8cebedf1a7a098e88a4db6bbc92b5191ec606ec
ksj0813 <tow369@naver.com>
2024-04-12 09:58:08 +0900

    baekjoon.11382

--- src/main/java/org/example/baekjoon1000/baekjoon/Main11382.java
+++ src/main/java/org/example/baekjoon1000/baekjoon/Main11382.java
@@ -1,17 +1,16 @@
-package org.example.baekjoon11382;
+package org.example.baekjoon1000.baekjoon;
 
 import java.util.Scanner;
-
-public class Main {
+public class Main11382 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         // Scanner(클래스) s(이름) = new(생성자) Scanner(System(클래스).in(함수/메서드))
-        int a,b,c;
-        a = s.nextInt();
-        b = s.nextInt();
-        c = s.nextInt();
+        long a,b,c;
+
+        a = s.nextLong();
+        b = s.nextLong();
+        c = s.nextLong();
+
         System.out.println(a+b+c);
-
-
     }
 }
