897c46487bd17062e68aa472f91f59b6465dfda3
ksj0813 <tow369@naver.com>
2024-04-12 09:59:18 +0900

    baekjoon.2588

--- src/main/java/org/example/baekjoon1000/baekjoon/Main2588.java
+++ src/main/java/org/example/baekjoon1000/baekjoon/Main2588.java
@@ -1,8 +1,8 @@
-package org.example.baekjoon2588;
+package org.example.baekjoon1000.baekjoon;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main2588 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
