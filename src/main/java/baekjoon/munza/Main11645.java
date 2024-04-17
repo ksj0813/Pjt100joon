f31133c8296c6d858e20f8a3fc1f6088064a4860
ksj0813 <tow369@naver.com>
2024-04-17 11:15:08 +0900

    baekjoon.11645

---
+++ src/main/java/baekjoon/munza/Main11645.java
@@ -0,0 +1,13 @@
+package baekjoon.munza;
+
+import java.util.Scanner;
+public class Main11645 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a = s.next().charAt(0);
+
+        System.out.print(a);
+    }
+}
